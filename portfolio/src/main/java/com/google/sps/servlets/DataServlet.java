7c34c4407ab0961a7c8e1ea15e965769ba5b0db8
beingPurple <nmorgan@sps-program.com>
2020-06-17 00:15:33 -0400

    adds comment section(prints to console but not to page)

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,19 +14,50 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Noelle!");
+//   @Override
+//   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//     response.setContentType("text/html;");
+//     response.getWriter().println("Hello Noelle!");
+//   }
+ private final List<String> commStream = new ArrayList<>();
+
+		@Override
+		public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+			
+			response.setContentType("application/json");
+			//   response.getWriter().println("Hello Noelle!");
+			String json = new Gson().toJson(commStream);
+			// String text = getParameter(request, "text-input", "");
+			System.out.println(commStream);
+			response.getWriter().println(json);
+		}
+		public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+			String text = getParameter(request, "textarea_field", "");//returns what is in text firld
+			commStream.add(text);
+            System.out.println(text);
+			System.out.println(commStream);
+			response.sendRedirect("/index.html");
+		}
+
+		private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -19,21 +19,31 @@
 function randMessage() {
     const msgList =
         ['“With a successful attack roll, the wizard maintains the thief in surfboard position.”',
-         'DM: So, what are your character’s flaws? Player: Acid reflux.',
-          'That’s too indiscriminate.  We should go with the rampaging bear.',
-          'We’ve managed to teach the robot about the most important human emotion: Humiliation.',
-          '“Do you have a permit for that facial hair?”',
-          'Cleric: “Everybody chill, it’s totes God’s will.”',
-          'Did asterisks exist in 2006?'];
+            'DM: So, what are your character’s flaws? Player: Acid reflux.',
+            'That’s too indiscriminate.  We should go with the rampaging bear.',
+            'We’ve managed to teach the robot about the most important human emotion: Humiliation.',
+            '“Do you have a permit for that facial hair?”',
+            'Cleric: “Everybody chill, it’s totes God’s will.”',
+            'Did asterisks exist in 2006?'];
     const msg = msgList[Math.floor(Math.random() * msgList.length)];
     const msgContainer = document.getElementById('message-container');
     msgContainer.innerText = msg;
 }
-async function servlet(){
-    const response = await fetch('/data');
-  const quote = await response.text();
-  document.getElementById('quote-container').innerText = quote;
+function servlet() {
+    fetch('/data').then(response => response.json())
+        .then((someTexts) => {
+            //   const quote = await response.text();
+            document.getElementById('quote-containter').innerText = someTexts;
+
+
+            // // Build the list of history entries.
+            // const historyEl = document.getElementById('quote-container');
+            // forEach((line) => {
+            //     historyEl.appendChild(createListElement(line));
+            // });
+        });
 }
+
 function addRandomGreeting() {
     const greetings =
         ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
